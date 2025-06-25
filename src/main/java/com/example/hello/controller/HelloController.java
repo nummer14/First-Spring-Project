7c74a5c8f83e13a2dc1672f0b4a01608bd77576e
numme <nummer14@naver.com>
2025-06-25 16:32:56 +0900

    First Commit

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,3 +1,4 @@
+
 package com.example.hello.controller;
 
 import org.springframework.stereotype.Controller;
@@ -5,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/")    // url부분
     public String hello() {
-        return "hello";
+        return "hello";   // → templates/hello.html을 렌더링
+    }
+
+    @GetMapping("/hidden-page")
+    public String hiddenPage() {
+        return "hidden-page";
     }
 }
